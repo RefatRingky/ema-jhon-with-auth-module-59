22021b6b3ccceae8f8013ab77595ee6b5b89df73
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-12 01:28:24 +0600

    form input field create

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,8 +3,19 @@ import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is Login Form</h2>
+        <div className='form-container'>
+           <div>
+           <h2 className='form-title'>Login</h2>
+            <div className='input-group'>
+               <label htmlFor='email'>Email</label>
+               <input type="email" name='' id=''></input>
+            </div>
+            <div className='input-group'>
+               <label htmlFor='password'>Password</label>
+               <input type="password" name='' id=''></input>
+            </div>
+           </div>
+        
         </div>
     );
 };
