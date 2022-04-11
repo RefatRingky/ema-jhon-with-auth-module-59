29cb5453e4439a69d2cff58f686cdfdc7e5b43d8
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-12 02:09:26 +0600

    event handler set

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
@@ -6,15 +7,22 @@ const Login = () => {
         <div className='form-container'>
            <div>
            <h2 className='form-title'>Login</h2>
+            <form action=''>
             <div className='input-group'>
                <label htmlFor='email'>Email</label>
-               <input type="email" name='' id=''></input>
+               <input type="email" name='' id='' required></input>
             </div>
             <div className='input-group'>
                <label htmlFor='password'>Password</label>
-               <input type="password" name='' id=''></input>
+               <input type="password" name='' id='' required></input>
             </div>
+            <input className='form-submit' type="submit" value="Login"></input>
+            </form>
+            <p className='link-position'>
+                New to Ema-Jhon? <Link className='form-link' to="/signup">Create an Account</Link>
+            </p>
            </div>
+           
         
         </div>
     );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUp = () => {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [confirmPassword,setConfirmPassword] = useState("");
+    const [error,setError] = useState("");
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event =>{
+        setPassword(event.target.value);
+    }
+    const handleconfirmPasswordBlur = event =>{
+        setConfirmPassword(event.target.value);
+    }
     return (
-        <div>
-            
+        <div className='form-container'>
+           <div>
+           <h2 className='form-title'>Sign Up</h2>
+            <form action=''>
+            <div className='input-group'>
+               <label htmlFor='email'>Email</label>
+               <input onBlur={handleEmailBlur} type="email" name='' id=''  required></input>
+            </div>
+            <div className='input-group'>
+               <label htmlFor='password'>Password</label>
+               <input onBlur={handlePasswordBlur} type="password" name='' id='' required></input>
+            </div>
+            <div className='input-group'>
+               <label htmlFor='confirm-password'>Confirm-Password</label>
+               <input onBlur={ handleconfirmPasswordBlur} type="password" name='confirm-password' id='' required></input>
+            </div>
+            <input className='form-submit' type="submit" value="Sign Up"></input>
+            </form>
+            <p className='link-position'>
+                Already have an account? <Link className='form-link' to="/login">Login</Link>
+            </p>
+           </div>
+           
+        
         </div>
     );
 };
