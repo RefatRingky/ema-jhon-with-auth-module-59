7d0f58f12e8195442b929d40bbb3b0a9508d0eb5
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-12 02:23:51 +0600

    createuserwithemailpassword import

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init'
 import './SignUp.css'
 
 const SignUp = () => {
@@ -7,6 +9,8 @@ const SignUp = () => {
     const [password,setPassword] = useState("");
     const [confirmPassword,setConfirmPassword] = useState("");
     const [error,setError] = useState("");
+
+    const [] = useCreateUserWithEmailAndPassword(auth)
     const handleEmailBlur = event =>{
         setEmail(event.target.value);
     }
@@ -16,11 +20,18 @@ const SignUp = () => {
     const handleconfirmPasswordBlur = event =>{
         setConfirmPassword(event.target.value);
     }
+    const handleCreateUser = event =>{
+        event.preventDefault();
+        if(password !== confirmPassword){
+            setError('Your two password did not match')
+            return;
+        }
+    }
     return (
         <div className='form-container'>
            <div>
            <h2 className='form-title'>Sign Up</h2>
-            <form action=''>
+            <form onSubmit={handleCreateUser}>
             <div className='input-group'>
                <label htmlFor='email'>Email</label>
                <input onBlur={handleEmailBlur} type="email" name='' id=''  required></input>
@@ -33,6 +44,7 @@ const SignUp = () => {
                <label htmlFor='confirm-password'>Confirm-Password</label>
                <input onBlur={ handleconfirmPasswordBlur} type="password" name='confirm-password' id='' required></input>
             </div>
+            <p style={{color:"red"}}>{error}</p>
             <input className='form-submit' type="submit" value="Sign Up"></input>
             </form>
             <p className='link-position'>
